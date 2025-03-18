64cbb28348e1ab3645126102b67f0baa270c2169
Peszto <halaszeszterke13@gmail.com>
2025-03-18 20:54:59 +0200

    Lab2

--- DatesAndStuff/test/DatesAndStuff.Tests/SimulationTimeTests.cs
+++ DatesAndStuff/test/DatesAndStuff.Tests/SimulationTimeTests.cs
@@ -5,17 +5,20 @@ namespace DatesAndStuff.Tests
         [OneTimeSetUp]
         public void OneTimeSetupStuff()
         {
-            //
+            // 
         }
+
         [SetUp]
         public void Setup()
         {
             // minden teszt felteheti, hogz elotte lefutott ez
         }
+
         [TearDown]
         public void TearDown()
         {
         }
+
         [OneTimeTearDown]
         public void OneTimeTearDown()
         {
@@ -23,7 +26,7 @@ namespace DatesAndStuff.Tests
 
         [Test]
         // Default time is not current time.
-        public void DefaultConstructor_CreatesTimeNotEqualToCurrentTime()
+        public void SimulationTime_Construction()
         {
             throw new NotImplementedException();
         }
@@ -34,33 +37,38 @@ namespace DatesAndStuff.Tests
         // <
         // >
         // <= different
-        // >= different
+        // >= different 
         // <= same
         // >= same
         // max
         // min
-        public void SimulationTime_Should_SupportComparisonOperators()
+        public void SimulationTime_Op()
         {
             throw new NotImplementedException();
         }
 
         private class TimeSpanArithmeticTests
         {
+
             [Test]
             // TimeSpanArithmetic
             // add
             // substract
             // Given_When_Then
-            // UserSignedIn_OrderSent_OrderIsRegistered
-            // DBB, specflow, cucumber, gherkin
-            public void BaseDateTime_AddingTimeSpan_ShiftsSimulationTime()
+            public void Addition_SimulationTimeIsShifted()
             {
+                // UserSignedIn_OrderSent_OrderIsRegistered
+                // DBB, specflow, cucumber, gherkin
+
                 // Arrange
                 DateTime baseDate = new DateTime(2010, 8, 23, 9, 4, 49);
                 SimulationTime sut = new SimulationTime(baseDate);
+
                 var ts = TimeSpan.FromMilliseconds(4544313);
+
                 // Act
                 var result = sut + ts;
+
                 // Assert
                 var expectedDateTime = baseDate + ts;
                 Assert.AreEqual(expectedDateTime, result.ToAbsoluteDateTime());
@@ -68,24 +76,25 @@ namespace DatesAndStuff.Tests
 
             [Test]
             //Method_Should_Then
-            // code kozelibb
-            // RegisterOrder_SignedInUserSendsOrder_OrderIsRegistered
-            public void SimulationTime_Should_AllowSubtraction()
+            public void Subtracttion_SimulationTimeShifted()
             {
+                // code kozelibb
+                // RegisterOrder_SignedInUserSendsOrder_OrderIsRegistered
                 throw new NotImplementedException();
             }
         }
 
+
         [Test]
         // simulation difference timespane and datetimetimespan is the same
-        public void TwoSimulationTimes_Subtracting_ProduceCorrectTimespan()
+        public void SimulationTime_SubtractSimulationTime()
         {
             throw new NotImplementedException();
         }
 
         [Test]
         // millisecond representation works
-        public void SimulationTime_Should_SupportMillisecondRepresentation()
+        public void SimulationTime_1msPerTick()
         {
             //var t1 = SimulationTime.MinValue.AddMilliseconds(10);
             throw new NotImplementedException();
@@ -93,7 +102,7 @@ namespace DatesAndStuff.Tests
 
         [Test]
         // next millisec calculation works
-        public void SimulationTime_IncrementingByMillisecond_UpdatesCorrectly()
+        public void SimulationTime_NextMillisec()
         {
             //Assert.AreEqual(t1.TotalMilliseconds + 1, t1.NextMillisec.TotalMilliseconds);
             throw new NotImplementedException();
@@ -101,28 +110,28 @@ namespace DatesAndStuff.Tests
 
         [Test]
         // creat a SimulationTime from a DateTime, add the same milliseconds to both and check if they are still equal
-        public void SimulationTime_Should_AllowAddingMilliseconds()
+        public void SimulationTime_AddMilliSeconds()
         {
             throw new NotImplementedException();
         }
 
         [Test]
         // the same as before just with seconds
-        public void SimulationTime_AddingSeconds_ShiftsTimeCorrectly()
+        public void SimulationTime_AddSeconds()
         {
             throw new NotImplementedException();
         }
 
         [Test]
         // same as before just with timespan
-        public void SimulationTime_Should_SupportTimeSpanAddition()
+        public void SimulationTime_AddTimeSpan()
         {
             throw new NotImplementedException();
         }
 
         [Test]
         // check string representation given by ToString
-        public void SimulationTime_ConvertingToString_ProducesCorrectRepresentation()
+        public void SimulationTime_ToString()
         {
             throw new NotImplementedException();
         }
